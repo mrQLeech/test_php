164040acf486df4839135d033dc0057f3b972306
QLeech <c-yleech@rambler.ru>
2016-03-30 23:49:56 +0500

    add task

--- test.php
+++ test.php
@@ -1,7 +1,20 @@
 <?php
 $arrPolindroms = ["Аргентина манит негра", "Sum summus mus"];
-echo ($arrPolindroms[0]);
-echo ($arrPolindroms[1]);
 
+function checkPolindrom($inString){
+    $str = cleanString($inString);
+    $revers = strrev($str);
+
+    return $str === $revers;
+}
+
+function cleanString($inString){
+    $clStr =  strtolower($inString);
+    return preg_replace('/\PL/u', '', $clStr);
+}
+
+echo 'revers 1 = ' . checkPolindrom($arrPolindroms[0]) . "\n";
+echo 'revers 2 = ' . checkPolindrom($arrPolindroms[1]) . " \r" ;
+echo 'revers 3 = ' . checkPolindrom('alpha romeo') . " \r" ;
 
 ?>
